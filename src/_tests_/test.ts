cd3bdc9885ebf2a08d9e382e81b66ad7af020296
rontam <rontam@swireproperties.com>
2021-12-01 11:04:07 +0800

    update logic and add testing config

---
+++ src/_tests_/test.ts
@@ -0,0 +1,7 @@
+import {BrowserStorage} from '../index';
+
+test('Browser Storage', () => {
+    expect(BrowserStorage.cookieStorage).toBeTruthy();
+    expect(BrowserStorage.localStorage).toBeTruthy();
+    expect(BrowserStorage.sessionStorage).toBeTruthy();
+});
---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import * as originalStore2 from 'store2';
+import * as cookiesJs from 'js-cookie';
+/* tslint:disable:no-namespace */
+declare namespace store {
+  export const local: StoreAPI;
+  export const session: originalStore2.StoreAPI;
+  export function set(key: string, value: any, expiration: number): any;
+
+  export interface StoreAPI {
+    set(key: string, value: any, expiration: number): any;
+  }
+}
+
+export const store2WithExtension: typeof store & typeof originalStore2 = originalStore2 as any;
+
+export class BrowserStorage {
+  static sessionStorage = originalStore2.session;
+  static localStorage = store2WithExtension.local;
+  static cookieStorage = cookiesJs;
+}
